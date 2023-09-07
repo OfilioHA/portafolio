8ad3ede66344676217dbf8b565eeb1c2ee35e7c5
Ofilio Herdocia Aráuz <Herdocia20@gmail.com>
2023-09-07 11:53:35 -0600

    Github API example

--- src/components/home/HeroSection/index.tsx
+++ src/components/home/HeroSection/index.tsx
@@ -1,15 +1,24 @@
-'use client';
+"use client";
+import { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 export function HeroSection() {
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    <h1>Hello World</h1>
-                </Col>
-                <Col></Col>
-            </Row>
-        </Container>
-    )
+  useEffect(() => {
+    fetch("https://api.github.com/users/OfilioHA/repos")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Hello World</h1>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
 }
