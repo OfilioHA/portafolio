6bfc376639739c24aa875d7bd151365ee4e9409a
OfilioHA <Herdocia20@gmail.com>
2023-09-15 15:51:21 -0600

    Fix: Brand using link next

--- src/components/utils/Header.tsx
+++ src/components/utils/Header.tsx
@@ -11,7 +11,7 @@ export function Header() {
         <header>
             <Navbar bg="primary" data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand href="/">Navbar</Navbar.Brand>
+                    <Navbar.Brand as={Link} href="/">Navbar</Navbar.Brand>
                     <Nav className="ms-auto" defaultActiveKey={pathName}>
                         <Nav.Link as={Link} href="/">Home</Nav.Link>
                         <Nav.Link as={Link} href="#features">Features</Nav.Link>
