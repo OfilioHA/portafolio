ad9f1ed07561f070b37bef39880064998dc7c987
Ofilio Herdocia Aráuz <Herdocia20@gmail.com>
2023-09-18 15:43:59 -0600

    Filter base on visibility type repos

--- src/components/home/RepositoriesSection/index.tsx
+++ src/components/home/RepositoriesSection/index.tsx
@@ -1,12 +1,26 @@
 "use client";
 import { Separator } from "@/components/utils/Separator";
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { Button, ButtonGroup, Col, Container, Row } from "react-bootstrap";
 import { RepositoryCard } from "./RepositoryCard";
 
 export function RepositoriesSection({ repositories }) {
   const [repos, setRepos] = useState(repositories);
   const [filter, setFilter] = useState("all");
+
+  const handleClickButtonFilter = useCallback((event) => {
+    const {
+      target: { value },
+    } = event;
+    setFilter(value);
+  }, []);
+
+  const reposFiletered = useMemo(() => {
+    return repos.filter((repo) => {
+      if (filter === "all") return true;
+      return repo.visibilityType === filter;
+    });
+  }, [filter]);
 
   return (
     <Container as="section" className="gap-90">
@@ -19,20 +33,28 @@ export function RepositoriesSection({ repositories }) {
           <div className="d-flex h-100 justify-content-end align-items-end">
             <div className="d-flex mb-3">
               <ButtonGroup>
-                <Button>Todos</Button>
-                <Button>Publicos</Button>
-                <Button>Privados</Button>
+                <Button onClick={handleClickButtonFilter} value="all">
+                  Todos
+                </Button>
+                <Button onClick={handleClickButtonFilter} value="public">
+                  Publicos
+                </Button>
+                <Button onClick={handleClickButtonFilter} value="private">
+                  Privados
+                </Button>
               </ButtonGroup>
             </div>
           </div>
         </Col>
       </Row>
       <Row>
-        {repos.map((repository, index) => (
-          <Col md={3} key={index}>
-            <RepositoryCard {...repository} />
-          </Col>
-        ))}
+        <Col sm={12}>
+          <div className="d-grid">
+            {reposFiletered.map((repository, index) => (
+              <RepositoryCard {...repository} key={index} />
+            ))}
+          </div>
+        </Col>
       </Row>
     </Container>
   );
