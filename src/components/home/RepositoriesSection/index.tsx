d0623b2f3938d178ef1c2bc264b9153c70e3665b
Ofilio Herdocia Aráuz <Herdocia20@gmail.com>
2023-09-18 14:42:35 -0600

    Repository card testing

--- src/components/home/RepositoriesSection/index.tsx
+++ src/components/home/RepositoriesSection/index.tsx
@@ -1,21 +1,35 @@
-'use client';
-
-import { useEffect, useState } from "react";
-import { Container, Row } from "react-bootstrap";
+"use client";
+import { Separator } from "@/components/utils/Separator";
+import { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { RepositoryCard } from "./RepositoryCard";
 
 export function RepositoriesSection({ repositories }) {
+  const [repos, setRepos] = useState(repositories);
+  const [filter, setFilter] = useState("all");
 
-    const [repos, setRepos] = useState(repositories);
-
-    useEffect(() => {
-        console.log(repos);
-    }, []);
-
-    return (<Container>
-        <Row>
-            {repos.map(({ name }, index) => (<h1 key={index}>
-                {name}</h1>
-            ))}
-        </Row>
-    </Container>);
+  return (
+    <Container as="section" className="gap-90">
+      <Row className="mb-5">
+        <Col md={6}>
+          <h2 className="mb-4">Conoce más sobre mi atraves de mis proyectos</h2>
+          <Separator />
+        </Col>
+        <Col md={6}>
+          <div className="d-flex h-100 justify-content-end align-items-end">
+            <Button>Todos</Button>
+            <Button>Publicos</Button>
+            <Button>Privados</Button>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        {repos.map((repository, index) => (
+          <Col md={3} key={index}>
+            <RepositoryCard {...repository} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
